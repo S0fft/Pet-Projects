6dd9d3fc48cef34f780d09f7bc242caae2a0f1a0
Pelikh Evgeniy <pelikh123.evg@gmail.com>
2023-01-28 08:47:36 +0100

    hangman game - finished

--- hangman_game.py
+++ hangman_game.py
@@ -5,12 +5,9 @@ current_word = f"{all_words[random.randint(0, len(all_words) - 1)]}"
 len_current_word = len(current_word)
 sum_of_latter = ""
 
-# -------------------------------------------------------------------------------------------------------------------------------
-
 print("Hangman - GAME")
 
-print(current_word)
-print(len_current_word)
+print(f"First latter of the word: {current_word[0]}, the last latter {current_word[-1]}")
 
 while True:
 
@@ -19,10 +16,17 @@ while True:
     if answer_latter in current_word:
         sum_of_latter += answer_latter
         len_current_word -= 1
-        print(current_word)
-        print(f"Lucky! Gussed right - {answer_latter.upper()}! Left {len_current_word}!")
+        print(f"Lucky! Gussed right latter - {answer_latter.upper()}! Left latters {len_current_word}!")
     else:
         print("Try again!")
 
-    if set(sum_of_latter) == set(current_word):
-        print("YOU GUSSED ALL THE RIGHT LATTER! REPLACE!")
+    if sum_of_latter == current_word:
+        print("YOU ARE WIN!")
+    elif set(sum_of_latter) == set(current_word):
+        print("YOU GUSSED ALl! REPLACE THE LATTERS!")
+        answer_word = input("Enter the word: ").lower()
+
+        if answer_word == current_word:
+            print("YOU ARE WIN!")
+        else:
+            print("You are lose!")
