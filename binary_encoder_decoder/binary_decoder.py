ff25342f26136fa249ad427d715822a647abb4bb
Pelikh Evgeniy <pelikh123.evg@gmail.com>
2023-01-29 13:05:02 +0100

    binary_encoder_decoder - update

--- binary_encoder_decoder/binary_decoder.py
+++ binary_encoder_decoder/binary_decoder.py
@@ -1,26 +1,37 @@
-answer_info = int("0b110110010101001000001100000001111100000", 2)
-print(answer_info)  # 466692211680
+answer_info = input("Data: ")  # 0b11001111111110101000111111010010001011101001110001100010100000100101011001000111010000100101100
 
-answer_range_key = int(input("Key - 1: "))
-answer_start_action_key = int(input("Key - 2: "))
-answer_final_action_key = int(input("Key - 3: "))
+answer_range_key = int(input("Key - 1: "))  # 7
+answer_factor_key = int(input("Key - 2: "))  # 3
+answer_division_key = int(input("Key - 3: "))  # 2
 
-result = (answer_info * answer_final_action_key) // answer_start_action_key
-print(result)
-
-latters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
+latters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
 num_of_latter = [*range(answer_range_key, answer_range_key + (len(latters)))]
 
-# dict_num_latters = dict(zip(num_of_latter, latters))
-# print(dict_num_latters)
+index_of_latter = [answer_factor_key * elem for elem in num_of_latter]
 
-# result_data = ""
+answer_info = int(answer_info, 2)
+answer_info *= answer_division_key
+answer_info = list(str(answer_info))
 
-# for i in answer_info:
-#     for k, v in dict_num_latters.items():
-#         if i == v:
-#             result_data += str(k)
+step = 0
 
-# result_data = int(result_data) * answer_final_action_key
+for i in index_of_latter:
+    step += len(str(i))
+    break
 
-# print(result_data)
+code_of_latter = []
+for i in range(0, len(answer_info)):
+    pass
+
+code_of_latter = [int(elem) for elem in code_of_latter]
+
+dict_num_latters = dict(zip(index_of_latter, latters))
+
+result_data = ""
+
+for num in code_of_latter:
+    for k, v in dict_num_latters.items():
+        if num == k:
+            result_data += v
+
+print(result_data)
--- binary_encoder_decoder/binary_encoder.py
+++ binary_encoder_decoder/binary_encoder.py
@@ -1,18 +1,15 @@
 answer_info = input("Data: ")
-print(answer_info)
+
 answer_range_key = int(input("Key - 1: "))
 answer_start_action_key = int(input("Key - 2: "))
 answer_final_action_key = int(input("Key - 3: "))
 
-latters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
+latters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
 num_of_latter = [*range(answer_range_key, answer_range_key + (len(latters)))]
-print(num_of_latter)
 
 num_of_latter = [answer_start_action_key * elem for elem in num_of_latter]
-print(num_of_latter)
 
 dict_num_latters = dict(zip(num_of_latter, latters))
-print(dict_num_latters)
 
 result_data = ""
 
@@ -21,7 +18,6 @@ for i in answer_info:
         if i == v:
             result_data += str(k)
 
-print(result_data)
 result_data = f"{bin(int(result_data) // answer_final_action_key)}"
 
 print(result_data)
