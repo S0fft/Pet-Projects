fa7f430faa75201587663aa54794fd1954a20906
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2023-09-12 13:06:57 +0200

    en grmammar edits again

--- binary_encoder_decoder/binary_decoder.py
+++ binary_encoder_decoder/binary_decoder.py
@@ -1,4 +1,5 @@
-from binary_encoder import answer_info, answer_range_key, answer_factor_key, answer_division_key, latters, num_range_of_latter, index_of_latter, dict_num_latters
+from binary_encoder import (answer_division_key, answer_info, dict_num_letters,
+                            index_of_latter)
 
 answer_info = int(answer_info, 2)
 answer_info *= answer_division_key
@@ -16,9 +17,8 @@ code_of_latter = [int(elem) for elem in code_of_latter]
 result_data = ""
 
 for num in code_of_latter:
-    for k, v in dict_num_latters.items():
+    for k, v in dict_num_letters.items():
         if k == num:
             result_data += v
 
 print(result_data)
-
--- binary_encoder_decoder/binary_encoder.py
+++ binary_encoder_decoder/binary_encoder.py
@@ -4,17 +4,17 @@ answer_range_key = int(input("Key - 1: "))
 answer_factor_key = int(input("Key - 2: "))
 answer_division_key = int(input("Key - 3: "))
 
-latters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
-num_range_of_latter = [*range(answer_range_key, answer_range_key + (len(latters)))]
+letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+num_range_of_latter = [*range(answer_range_key, answer_range_key + (len(letters)))]
 
 index_of_latter = [answer_factor_key * elem for elem in num_range_of_latter]
 
-dict_num_latters = dict(zip(index_of_latter, latters))
+dict_num_letters = dict(zip(index_of_latter, letters))
 
 result_data = ""
 
 for i in answer_info:
-    for k, v in dict_num_latters.items():
+    for k, v in dict_num_letters.items():
         if i == v:
             result_data += str(k)
 
