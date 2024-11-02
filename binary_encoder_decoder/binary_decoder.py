0cb8961c79d572029f1f8f2ccf7ed50c150dde87
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-02 23:01:04 +0100

    pep8 fix

--- binary_encoder_decoder/binary_decoder.py
+++ binary_encoder_decoder/binary_decoder.py
@@ -1,5 +1,4 @@
-from binary_encoder import (answer_division_key, answer_info, dict_num_letters,
-                            index_of_latter)
+from binary_encoder import answer_division_key, answer_info, dict_num_letters, index_of_latter
 
 answer_info = int(answer_info, 2)
 answer_info *= answer_division_key
